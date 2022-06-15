268254f1796cbaab54592cec89f89969a1683f1e
zeynab Aghaamini <zeinabam14@gmail.com>
2022-06-15 23:34:56 +0430

    commit 5

--- app/src/main/java/ir/shariaty/ganjo/GetData.java
+++ app/src/main/java/ir/shariaty/ganjo/GetData.java
@@ -1,8 +1,6 @@
 package ir.shariaty.ganjo;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
-
 public interface GetData {
     String BASE_URL = "https://ganjgah.ir/api/ganjoor/hafez/";
     @GET("Faal")
